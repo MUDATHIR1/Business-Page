8b96ed409d5e70d436c467853b08e57f105b7c21
MUDATHIR1 <88151040+MUDATHIR1@users.noreply.github.com>
2024-02-01 14:06:23 +0200

    l
    kl

---
+++ web-dev-env-main/web-dev-env-main/src/hello.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Testing</title>
+</head>
+<body>
+
+<!-- this is used foor html comments
+ <?php
+ // this is printing helo world 
+echo"<h1>Hello World !</h1>";
+?>   
+
+
+</body>
+</html>
+
+
